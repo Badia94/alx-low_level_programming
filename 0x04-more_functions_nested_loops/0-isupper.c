e9440c253d67a16db95f0762738111c4403428f7
Badia94 <badialamkaddem@gmail.com>
2023-03-24 16:53:33 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,14 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+*_isupper - function that verifies if a  character is uppercase or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 
-int main(void)
+int _isupper(int c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
 return (0);
 }
-
